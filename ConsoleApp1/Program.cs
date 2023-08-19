95ccbdc9d3379a4cd222b0f04e3f73dd6e1464b0
atul-9 <atul09agrawal@gmail.com>
2023-08-19 14:21:41 +0530

    file Structure made

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,23 +13,11 @@ namespace ConsoleApp1
         {
             Products p= new Products();
            ConnectToDB c=new ConnectToDB();
-           
-//            mysql_host = "localhost"
-//mysql_user = "root"
-//mysql_password = "xyz"
-//mysql_database = "javaproject"
-            //conn = mysql.connector.connect(
-            //host = MYSQL_HOST,
-            //user = MYSQL_USER,
-            //password = MYSQL_PASSWORD,
-            //database = MYSQL_DATABASE
-            //)
+
             try
             {
                 c.Connect();
-                //Console.WriteLine("Enter ProductId");
-                //p.ProductId = Convert.ToInt32(Console.ReadLine());
-                //Console.WriteLine("Product Id is Valid");
+              
             }
             catch (Exception ex)
 
---
+++ DataAccessLayer/CommonClass.cs
@@ -0,0 +1,30 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DataAccessLayer
+{
+    internal static class CommonClass
+    {
+        public static void Connect()
+        {
+
+            string cnstring = "server=localhost;userid=root;password=xyz;database=shopping";
+            MySqlConnection cn = new MySqlConnection(cnstring);
+            MySqlCommand cmd = new MySqlCommand("select * from products", cn);
+            cn.Open();
+            MySqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Console.WriteLine(dr[s0] + "\t" + dr[1] + "\t" + dr[2]);
+
+
+            }
+            cn.Close();
+
+
+        }
+    }
+}
