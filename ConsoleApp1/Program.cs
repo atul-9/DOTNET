358ab529e36655758b602569c1b5554c83da0c5c
atul-9 <atul09agrawal@gmail.com>
2023-08-19 10:23:24 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 10;
+            float r = 3.4577f;
+            double d1 = 54565.545d;
+            decimal c1 = 589232323.6545654m;
+
+
+            Console.WriteLine(r);
+            Console.WriteLine(cnt);
+
+        }
+    }
+}
